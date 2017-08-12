83ea9213cda2e3539453315b345fa7693a4e573a
Jens Fischer <JensFZ@gmail.com>
2017-08-12 11:07:02 +0200

    firts functionality implemented

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+require('dotenv').config();
 var ddns = require("cloudflare-dynamic-dns");
+
+var email = process.env.CLOUDFLARE_EMAIL;
+var key = process.env.CLOUDFLARE_API_KEY;
+var hostname = process.env.CLOUDFLARE_HOSTNAME;
+var zonename = process.env.CLOUDFLARE_ZONENAME;
+
+var options = {
+    auth: {
+        email: email,
+        key: key
+    },
+    ip: "93.184.216.34",
+    recordName: hostname,
+    zoneName: zonename
+};
+
+ddns.update(options, function(err) {
+    if (err) {
+        console.log("An error occurred:");
+        console.log(err);
+    } else {
+        console.log("Success!");
+    }
+});
