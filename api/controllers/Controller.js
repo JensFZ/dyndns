8431318140a7ce56487621050e31c8be42eaacb2
Jens Fischer <JensFZ@gmail.com>
2017-09-18 17:52:37 +0200

    next nodejs step

--- api/controllers/Controller.js
+++ api/controllers/Controller.js
@@ -3,41 +3,56 @@ require('dotenv').config();
 
 var ddns = require("cloudflare-dynamic-dns");
 var mongoose = require('mongoose'),
-  UserSchema = mongoose.model('UserSchema');
+    UserSchema = mongoose.model('UserSchema');
+var email = process.env.CLOUDFLARE_EMAIL;
+var key = process.env.CLOUDFLARE_API_KEY;
 
 exports.create_domain = function(req, res) {
+    var hostname = process.env.CLOUDFLARE_HOSTNAME;
+    var zonename = process.env.CLOUDFLARE_ZONENAME;
 
+    var options = {
+        auth: {
+            email: email,
+            key: key
+        },
+        recordName: hostname,
+        zoneName: zonename
+    };
+
+    ddns.update(options, function(err) {
+        if (err) {
+            console.log("An error occurred:");
+            console.log(err);
+        } else {
+            console.log("Success!");
+        }
+    });
 };
 
 exports.update_domain = function(req, res) {
+    var hostname = process.env.CLOUDFLARE_HOSTNAME;
+    var zonename = process.env.CLOUDFLARE_ZONENAME;
 
+    var options = {
+        auth: {
+            email: email,
+            key: key
+        },
+        recordName: hostname,
+        zoneName: zonename
+    };
+
+    ddns.update(options, function(err) {
+        if (err) {
+            console.log("An error occurred:");
+            console.log(err);
+        } else {
+            console.log("Success!");
+        }
+    });
 };
 
 exports.delete_domain = function(req, res) {
 
 };
-
-/*
-var email = process.env.CLOUDFLARE_EMAIL;
-var key = process.env.CLOUDFLARE_API_KEY;
-var hostname = process.env.CLOUDFLARE_HOSTNAME;
-var zonename = process.env.CLOUDFLARE_ZONENAME;
-
-var options = {
-    auth: {
-        email: email,
-        key: key
-    },
-    recordName: hostname,
-    zoneName: zonename
-};
-
-ddns.update(options, function(err) {
-    if (err) {
-        console.log("An error occurred:");
-        console.log(err);
-    } else {
-        console.log("Success!");
-    }
-});
-*/
